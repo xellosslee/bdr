543b40197da35b427c1bc756e8e6a188b995757b
이근석 <xellossmail@gmail.com>
2023-11-16 16:26:30 +0900

    ecosystem file added

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	apps: [
+		{
+			namespace: 'bdr',
+			name: 'bdr-server',
+			script: 'server.mjs',
+			cwd: './',
+			max_restarts: 5,
+			env: {
+				NODE_ENV: 'prod',
+			},
+			instances: 0,
+			exec_mode: 'cluster',
+			wait_ready: true,
+			listen_timeout: 50000,
+			kill_timeout: 5000,
+		},
+	],
+}
---
+++ ecosystem.local.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	apps: [
+		{
+			namespace: 'bdr',
+			name: 'bdr-server',
+			script: 'server.mjs',
+			cwd: './',
+			autorestart: false,
+			watch: ['server.mjs', 'models', 'mysql.mjs', 'src'],
+		},
+	],
+}
