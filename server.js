96b6943d8cd07281b664c66d980188a4db86eb50
이근석 <gs.lee@cndfactory.com>
2023-11-19 13:45:32 +0900

    put api 저장 or 수정 로직 추가

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const ejs = require('ejs')
 
 const defaultErrorHtml = `<p>일시적으로 장애가 발생할 수 있습니다.</p>
                         <p>잠시 후 다시 시도해 주세요.</p>`
+const defJsonError = { status: 500, code: '99', message: '오류가 발생하였습니다.' }
+const jsonSuccess = { code: '00', message: '정상입니다.' }
 
 consoleStamp(console, 'yyyy-mm-dd HH:MM:ss.l')
 
@@ -43,13 +45,46 @@ process.on('SIGINT', async () => {
 	})
 })
 
+// 데이터 저장
 server.post('/item/put', async function (req, res) {
-	console.dir(req.body)
-	await DB.Item.create(req.body.finalItem)
-	await DB.Earn.bulkCreate(req.body.finalItem.earnList.map((e) => ({ ...e, itemId: req.body.finalItem.itemId })))
-	await DB.Usages.bulkCreate(req.body.finalItem.usageList.map((e) => ({ ...e, useItemId: req.body.finalItem.itemId })))
-
-	res.send(200, 'ok')
+	let transaction = await DB.transaction()
+	try {
+		console.dir(req.body)
+		if (req.body.item.itemId != null) {
+			await DB.Item.update(req.body.item, { where: req.body.item.itemId, transaction })
+			await DB.Earn.destroy({ where: { itemId: req.body.item.itemId, transaction } })
+			await DB.Earn.bulkCreate(
+				req.body.item.earnList.map((e) => ({ ...e, itemId: req.body.item.itemId })),
+				{ transaction },
+			)
+			await DB.Usages.destroy({ where: { itemId: req.body.item.itemId }, transaction })
+			await DB.Usages.bulkCreate(
+				req.body.item.usageList.map((e) => ({ ...e, useItemId: req.body.item.itemId })),
+				{ transaction },
+			)
+		} else {
+			let item = await DB.Item.create(req.body.item, { transaction })
+			if (item.itemId) {
+				req.body.item.itemId = item.itemId
+			}
+			await DB.Earn.bulkCreate(
+				req.body.item.earnList.map((e) => ({ ...e, itemId: req.body.item.itemId })),
+				{ transaction },
+			)
+			await DB.Usages.bulkCreate(
+				req.body.item.usageList.map((e) => ({ ...e, useItemId: req.body.item.itemId })),
+				{ transaction },
+			)
+		}
+		res.send(200, { ...jsonSuccess })
+	} catch (err) {
+		console.error(err.original || err)
+		if (typeof err == 'object') {
+			res.send({ ...defJsonError, ...err })
+		} else {
+			res.send(defJsonError)
+		}
+	}
 })
 
 server.get('/item/:itemId', async (req, res) => {
