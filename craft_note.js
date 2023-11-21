9e5cdb8769328fa714347b0df6ebc8599d3feb27
이근석 <gs.lee@cndfactory.com>
2023-11-22 01:25:39 +0900

    랜덤 조회에서는 성공하도록 예외처리

--- craft_note.js
+++ craft_note.js
@@ -26,7 +26,8 @@ router.get('/', (req, res, next) => {
 	DB.Item.findOne({ attributes: ['itemId'], order: [sq.fn('rand')] }).then((item) => {
 		let url = '/item/' + encode(JSON.stringify({ itemId: item.itemId }))
 		// res.redirect(url, next)
-		req.params.itemId = item.itemId
+		req.params.itemId = item.itemId.toString()
+		req.internal = true
 		itemPageIn(req, res)
 	})
 })
@@ -91,7 +92,8 @@ async function itemPageIn(req, res) {
 			item = items[0]
 		}
 		// 운영에서는 숫자 조회 불가능하게 방지
-		if (!item && process.env.NODE_ENV != 'prod') {
+		// 단, 랜덤 조회 일때에는 성공
+		if (!item && !req.internal && process.env.NODE_ENV != 'prod') {
 			if (!Number.isNaN(Number(params.itemId))) {
 				item = await DB.Item.findOne({
 					include: [{ model: DB.File, as: 'itemImage', attributes: ['imgUrl'] }],
