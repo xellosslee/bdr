20bddd935daa476055e8c809ec3eb0216d4dce2e
이근석 <xellossmail@gmail.com>
2023-11-22 11:17:04 +0900

    id 컬럼 추가 item 맵핑용 resultItemCd값 추가 추후 resultItemId는 제거 필요

--- models/usages.js
+++ models/usages.js
@@ -4,6 +4,11 @@ class Usages extends Model {}
 module.exports = (sq) => {
 	Usages.init(
 		{
+			id: {
+				type: DataTypes.BIGINT.UNSIGNED,
+				primaryKey: true,
+				allowNull: false,
+			},
 			itemId: {
 				type: DataTypes.BIGINT.UNSIGNED,
 				comment: '재료 아이템',
@@ -12,13 +17,17 @@ module.exports = (sq) => {
 				type: DataTypes.BIGINT.UNSIGNED,
 				comment: '제작 가능 아이템',
 			},
+			resultItemCd: {
+				type: DataTypes.BIGINT.UNSIGNED,
+				comment: '제작 가능 아이템 코드',
+			},
 		},
 		{
 			sequelize: sq,
 			tableName: 'usages',
 			charset: 'utf8mb4',
 			collate: 'utf8mb4_0900_ai_ci',
-			indexes: [{ fields: ['itemId', 'resultItemId'] }],
+			indexes: [{ fields: ['itemId', 'resultItemId'] }, { fields: ['itemId', 'resultItemCd'] }],
 		},
 	)
 	return Usages
