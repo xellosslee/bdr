9589e5d6fd1220e80ef4127cf0898d49bb5f2ec4
이근석 <xellossmail@gmail.com>
2023-11-23 14:04:29 +0900

    priority관련 제거

--- cron.js
+++ cron.js
@@ -3,16 +3,7 @@ const { DB, sq, Op } = require('./mysql')
 
 // 새벽 4시
 cron.schedule('0 4 * * *', negativeItemRemove)
-// 매 시각 0분
-cron.schedule('0 * * * *', updatePriority)
 async function negativeItemRemove(time) {
-	try {
-		await DB.Item.update({ removed: 1 }, { where: { likeCount: { [Op.lt]: 0 } } })
-	} catch (err) {
-		console.error('negativeItemRemove', err)
-	}
-}
-async function updatePriority(time) {
 	try {
 		await DB.Item.update({ removed: 1 }, { where: { likeCount: { [Op.lt]: 0 } } })
 	} catch (err) {
