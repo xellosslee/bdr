f974b7d644c54f47378c409eac7e077539743ac5
이근석 <gs.lee@cndfactory.com>
2023-11-29 00:40:13 +0900

    팝업 닫기 버튼 디자인 수정 팝업 내부 flex 디자인 수정

--- front/src/lib/components/LayerPopup.svelte
+++ front/src/lib/components/LayerPopup.svelte
@@ -45,53 +45,55 @@
 <div class="layerPopup" transition:fade={{ duration: 300 }}>
 	<button class="dimmed" on:click={dimmedClick} aria-roledescription="close btn" />
 	<div class="box">
-		<button class="btn closeBtn" on:click={closeEditLayer}><i class="icon ic16 icon-close" /></button>
-		<div class="inputWrap name">
-			<div class="inputTitle">아이템명 <input type="text" class="label" value={popupItem?.name} /></div>
-		</div>
-		<ImageSearch bind:popupItem />
-		<div class="inputWrap imageUpload">
-			<div class="inputTitle">
-				이미지 파일 업로드<input type="file" on:change={setImageName} /><input type="text" value={uploadImage.name || ''} /><button on:click={doUpload}>doUpload</button>
+		<button class="closeBtn" on:click={closeEditLayer}><i class="icon ic16 icon-close" /></button>
+		<div class="box-container">
+			<div class="inputWrap name">
+				<div class="inputTitle">아이템명 <input type="text" class="label" value={popupItem?.name} /></div>
 			</div>
-		</div>
-		<div class="inputWrap desc">
-			<div class="inputTitle">아이템 설명<textarea>{popupItem.desc.replace(/<br>/gi, '\n')}</textarea></div>
-		</div>
-		<div class="inputWrap crafts">
-			<div class="inputTitle">획득 방법</div>
-			<ul>
-				<li>
-					<div>동작 <input type="text" value="요리" /></div>
-					<ul>
-						<li>
-							<div>아이템 <input type="text" /></div>
-							<div>개수 <input type="text" /></div>
-						</li>
-						<li>
-							<div>아이템 <input type="text" /></div>
-							<div>개수 <input type="text" /></div>
-						</li>
-						<li>
-							<div>아이템 <input type="text" /></div>
-							<div>개수 <input type="text" /></div>
-						</li>
-						<li>
-							<div>아이템 <input type="text" /></div>
-							<div>개수 <input type="text" /></div>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</div>
-		<div class="inputWrap usages">
-			<div class="inputTitle">제작가능 아이템<button class="btn"><i class="icon ic16 icon-add" />추가</button></div>
-			<ul>
-				<li>
-					<div>아이템 선택</div>
-					<button class="btn"><i class="icon ic16 icon-del" />삭제</button>
-				</li>
-			</ul>
+			<ImageSearch bind:popupItem />
+			<div class="inputWrap imageUpload">
+				<div class="inputTitle">
+					이미지 파일 업로드<input type="file" on:change={setImageName} /><input type="text" value={uploadImage.name || ''} /><button on:click={doUpload}>doUpload</button>
+				</div>
+			</div>
+			<div class="inputWrap desc">
+				<div class="inputTitle">아이템 설명<textarea>{popupItem.desc.replace(/<br>/gi, '\n')}</textarea></div>
+			</div>
+			<div class="inputWrap crafts">
+				<div class="inputTitle">획득 방법</div>
+				<ul>
+					<li>
+						<div>동작 <input type="text" value="요리" /></div>
+						<ul>
+							<li>
+								<div>아이템 <input type="text" /></div>
+								<div>개수 <input type="text" /></div>
+							</li>
+							<li>
+								<div>아이템 <input type="text" /></div>
+								<div>개수 <input type="text" /></div>
+							</li>
+							<li>
+								<div>아이템 <input type="text" /></div>
+								<div>개수 <input type="text" /></div>
+							</li>
+							<li>
+								<div>아이템 <input type="text" /></div>
+								<div>개수 <input type="text" /></div>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div class="inputWrap usages">
+				<div class="inputTitle">제작가능 아이템<button class="btn"><i class="icon ic16 icon-add" />추가</button></div>
+				<ul>
+					<li>
+						<div>아이템 선택</div>
+						<button class="btn"><i class="icon ic16 icon-del" />삭제</button>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
@@ -107,13 +109,13 @@
 		height: 100%;
 	}
 
-	.layerPopup .dimmed {
+	.dimmed {
 		background: rgba(0, 0, 0, 0.5);
 		width: 100%;
 		height: 100%;
 	}
 
-	.layerPopup .box {
+	.box {
 		position: absolute;
 		background: white;
 		border: 1px solid #333;
@@ -129,13 +131,21 @@
 		margin: auto;
 	}
 
-	.layerPopup .closeBtn {
+	.closeBtn {
 		position: absolute;
-		top: 0;
-		right: 0;
-		width: 24px;
-		height: 24px;
+		top: 10px;
+		right: 10px;
 		padding: 10px;
+	}
+
+	.box-container {
+		padding: 20px;
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.box-container > div {
+		flex: 1 0 auto;
 	}
 
 	@keyframes showAni {
