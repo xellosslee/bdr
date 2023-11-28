81b3836d1ebb55574241a16c23db218545216323
이근석 <xellossmail@gmail.com>
2023-11-28 11:18:30 +0900

    화면이 뜨게만 일단 수정

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -3,8 +3,9 @@
 	import logo from '$lib/img/blackSpirit.png'
 	import { onMount } from 'svelte'
 	let items = null
+	let popupItem = null
 	onMount(async function () {
-		let res = await lib.api({ url: 'http://127.0.0.1:7700/items/get' })
+		let res = await lib.api({ url: '/items/get' })
 		let r = await res.json()
 		if (r.code == '00') {
 			items = r.data
