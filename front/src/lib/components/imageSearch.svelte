5451d8609b710f913104eb966006ee69de402266
이근석 <xellossmail@gmail.com>
2023-11-28 13:57:07 +0900

    이미지 검색 box 별도 svelte 파일로 이동!

---
+++ front/src/lib/components/imageSearch.svelte
@@ -0,0 +1,49 @@
+<script>
+	import lib from '$lib'
+	export let popupItem = {}
+	let popupImageList = [] // 팝업 검색 결과 (최대 10개) 노출용 리스트
+	let searchImageText = '' // 팝업의 이미지 검색 기록 저장용 temp 변수
+	let popupImageSearch = ''
+	async function imageSearch(event) {
+		let name = event.target.value
+		console.log('searchText', name)
+		if (name == '') {
+			return
+		}
+		var pattern = /([^가-힣\x20])/i
+		if (pattern.test(name)) {
+			console.log('자음,모음만 있는 경우 검색 안함')
+			return
+		}
+		if (searchImageText == name) {
+			console.log('지난 검색 단어와 같은 경우 무시')
+			return
+		}
+		searchImageText = name
+		let res = await lib.api({ url: '/file/list', data: { name, page: 0 } })
+		let result = await res.json()
+		console.log(result)
+		popupImageList = result.data.rows
+	}
+	function chooseImage(event) {
+		popupItem.itemImage.name = popupImageList[event.currentTarget.dataset.imageIdx].name
+		popupItem.itemImage.fileId = popupImageList[event.currentTarget.dataset.imageIdx].fileId
+		// 검색 초기화
+		popupImageList = []
+		searchImageText = ''
+		popupImageSearch = ''
+	}
+</script>
+
+<div class="props.class">
+	<div class="inputTitle">
+		이미지 검색 <input list="image-list" on:keyup={imageSearch} value={popupImageSearch} />
+		{#if popupItem.itemImage.name}<div>선택된 이미지 : {popupItem.itemImage.name}</div>{/if}
+	</div>
+	{#each popupImageList as popupImage, i}
+		<button on:click={chooseImage} data-image-idx={i}><img src={popupImage.imgUrl ? 'http://127.0.0.1:7700' + popupImage.imgUrl : ''} alt={popupImage.name} />{popupImage.name}</button>
+	{/each}
+</div>
+
+<style>
+</style>
--- front/src/routes/[itemId]/+page.svelte
+++ front/src/routes/[itemId]/+page.svelte
@@ -4,12 +4,10 @@
 	import { fade } from 'svelte/transition'
 	import { onMount } from 'svelte'
 	import { page } from '$app/stores'
+	import ImageSearch from '$components/imageSearch.svelte'
 	let items = null // 화면 랜딩된 아이템 전체 정보. Earns, Usages
 	let popupItem = null // 열려있는 팝업의 아이템 정보
 	let uploadImage = {} // 업로드 대상 이미지 값 저장용
-	let popupImageList = [] // 팝업 검색 결과 (최대 10개) 노출용 리스트
-	let searchImageText = '' // 팝업의 이미지 검색 기록 저장용 temp 변수
-	let popupImageSearch = ''
 	onMount(async function () {
 		let res = await lib.api({ url: '/items/get/' + $page.params.itemId })
 		let r = await res.json()
@@ -54,31 +52,6 @@
 		} else {
 			uploadImage = {}
 		}
-	}
-	async function imageSearch(event) {
-		let name = event.target.value
-		console.log('searchText', name)
-		if (name == '') {
-			return
-		}
-		var pattern = /([^가-힣\x20])/i
-		if (pattern.test(name)) {
-			console.log('자음,모음만 있는 경우 검색 안함')
-			return
-		}
-		searchImageText = name
-		let res = await lib.api({ url: '/file/list', data: { name, page: 0 } })
-		let result = await res.json()
-		console.log(result)
-		popupImageList = result.data.rows
-	}
-	function chooseImage(event) {
-		popupItem.itemImage.name = popupImageList[event.currentTarget.dataset.imageIdx].name
-		popupItem.itemImage.fileId = popupImageList[event.currentTarget.dataset.imageIdx].fileId
-		// 검색 초기화
-		popupImageList = []
-		searchImageText = ''
-		popupImageSearch = ''
 	}
 </script>
 
@@ -212,15 +185,7 @@
 			<div class="inputWrap name">
 				<div class="inputTitle">아이템명 <input type="text" class="label" value={popupItem?.name} /></div>
 			</div>
-			<div class="inputWrap image">
-				<div class="inputTitle">
-					아이템 이미지 선택<input list="image-list" on:keyup={imageSearch} value={popupImageSearch} />
-					{#if popupItem.itemImage.name}<div>선택된 이미지 : {popupItem.itemImage.name}</div>{/if}
-				</div>
-				{#each popupImageList as popupImage, i}
-					<button on:click={chooseImage} data-image-idx={i}><img src={popupImage.imgUrl ? 'http://127.0.0.1:7700' + popupImage.imgUrl : ''} alt={popupImage.name} />{popupImage.name}</button>
-				{/each}
-			</div>
+			<ImageSearch bind:popupItem />
 			<div class="inputWrap imageUpload">
 				<div class="inputTitle">
 					이미지 파일 업로드<input type="file" on:change={setImageName} /><input type="text" value={uploadImage.name || ''} /><button on:click={doUpload}>doUpload</button>
