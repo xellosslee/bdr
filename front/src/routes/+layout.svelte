c6145730e486ad692626da5ff7381dd4663f0937
이근석 <xellossmail@gmail.com>
2023-12-01 10:17:08 +0900

    화면 전역 단축키 지정 (내부 component제어 방법 찾는 중)

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
 	import '$lib/css/default.css'
 	import '$lib/css/icons.css'
+	function handleKeyDown(event) {
+		if (event.key === 'Escape') {
+			escape()
+		}
+	}
+	function escape() {
+		console.debug('닫기 수행')
+	}
 </script>
 
+<svelte:window on:keydown={handleKeyDown} />
 <slot />
