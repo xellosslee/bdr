90587b13a9c71b2930a3a842d5e86b7ea7642ed1
이근석 <gs.lee@cndfactory.com>
2023-12-10 11:24:45 +0900

    bigint 데이터 전송 되도록 수정

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,10 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+// bigint로 된 컬럼을 json으로 전달 할 때 string 자료형으로 전달하도록 일괄 적용
+// response data에서 string으로 뽑아서 써야 함
+BigInt.prototype.toJSON = function () {
+	return this.toString()
+}
 export default {
 	api: async function api(param) {
 		let body
